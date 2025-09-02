a407aa696d8726b799cfd3fc9f4a95ecaaebf129
karthik <220020026@iitdh.ac.in>
2025-09-03 00:05:52 +0530

    added Lab-5 Task -1

--- main.c
+++ main.c
@@ -3,36 +3,35 @@
 #include <stdbool.h>
 #include "tm4c123gh6pm.h"
 
-#define red_led (1<<1)
-#define blue_led (1<<2)
-#define green_led (1<<3)
+#define stctrl  (*((volatile uint32_t *)0xE000E010))
+#define streload  (*((volatile uint32_t *)0xE000E014))
+#define stcurrent  (*((volatile uint32_t *)0xE000E018))
 
-int main(void)
-{
+#define enable 1
+#define clk_source (1<<2)
+#define count (1<<16)
+#define red_led (1<<1)
+#define sw_2 (1)
+
+int main(void){
+
     SYSCTL_RCGC2_R |=   0x00000020;
     GPIO_PORTF_LOCK_R = 0x4C4F434B;
     GPIO_PORTF_CR_R = 0x1F;
     GPIO_PORTF_DEN_R = 0x1F;
     GPIO_PORTF_DIR_R = 0X0E;
-    GPIO_PORTF_PUR_R = 0x11;
-
-    GPIO_PORTF_DATA_R = 0x00;
+    GPIO_PORTF_PUR_R = 0X11;
+    streload = 0xFFFFFF;
+    stctrl |= clk_source;
+    GPIO_PORTF_DATA_R = 0x0;
 
     while(1){
+        stctrl |= enable;
+       while ((stctrl & count) == 0);
+        GPIO_PORTF_DATA_R ^= red_led;
+        //// disable timer
+        stctrl &= ~enable;
+        stcurrent = 0x0;
 
-        if((GPIO_PORTF_DATA_R & 0x11) == 0x10){
-            GPIO_PORTF_DATA_R |= red_led;
-        }
-        else if ((GPIO_PORTF_DATA_R & 0x11)==0x01){
-            GPIO_PORTF_DATA_R |= blue_led;
-        }
-        else if ((GPIO_PORTF_DATA_R & 0x11) == 0x00){
-            GPIO_PORTF_DATA_R |= green_led;
-        }
-        else{
-            GPIO_PORTF_DATA_R = 0x00;
-        }
     }
-
-	return 0;
 }
