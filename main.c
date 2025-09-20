bf856c89242d748a1ba78d24f14c8eff4aca94ce
karthik <220020026@iitdh.ac.in>
2025-09-20 18:38:35 +0530

    added lab-6 task 1

--- main.c
+++ main.c
@@ -9,41 +9,32 @@
 
 #define enable 1
 #define clk_source (1<<2)
-#define count (1<<16)
+#define INTEN (1<<1)
 #define red_led (1<<1)
 #define blue_led (1<<2)
 #define sw_1 (1<<4)
 
-int main(void){
 
+void systick_handler(void){
+    GPIO_PORTF_DATA_R ^= red_led;
+
+}
+
+void main(void)
+{
     SYSCTL_RCGC2_R |=   0x00000020;
-    GPIO_PORTF_LOCK_R = 0x4C4F434B;
-    GPIO_PORTF_CR_R = 0x1F;
-    GPIO_PORTF_DEN_R = 0x1F;
-    GPIO_PORTF_DIR_R = 0X0E;
-    GPIO_PORTF_PUR_R = 0X11;
-    streload = 0xFFFFFF;
-    stctrl |= clk_source;
-    GPIO_PORTF_DATA_R = 0x0;
+   GPIO_PORTF_LOCK_R = 0x4C4F434B;
+   GPIO_PORTF_CR_R = 0x1F;
+   GPIO_PORTF_DEN_R = 0x1F;
+   GPIO_PORTF_DIR_R = 0X0E;
+   GPIO_PORTF_PUR_R = 0X11;
+   GPIO_PORTF_DATA_R = 0x0;
+   stcurrent = 0x0;
+   stctrl =0x0;
+   streload =0xFFFFFF;
+   stctrl |=(enable | clk_source | INTEN);
 
-    while(1){
-        toggle_blue();
-       stctrl |= enable;
-       while ((stctrl & count) == 0);
-        GPIO_PORTF_DATA_R ^= red_led;
-        //// disable timer
-        stctrl &= ~enable;
-        stcurrent = 0x0;
 
-    }
+      while (1);
+
 }
-
-void toggle_blue(void){
-    if((GPIO_PORTF_DATA_R & sw_1 ) == 0x00){
-        GPIO_PORTF_DATA_R |= blue_led;
-    }
-    else {
-        GPIO_PORTF_DATA_R &= ~blue_led;
-    }
-}
-
