37caefe6931d6a107934fba58fc25739ef7b5ba9
karthik <220020026@iitdh.ac.in>
2025-09-20 19:18:49 +0530

    added lab6 task2

--- main.c
+++ main.c
@@ -3,38 +3,40 @@
 #include <stdbool.h>
 #include "tm4c123gh6pm.h"
 
-#define stctrl  (*((volatile uint32_t *)0xE000E010))
-#define streload  (*((volatile uint32_t *)0xE000E014))
-#define stcurrent  (*((volatile uint32_t *)0xE000E018))
+#define RED_LED   (1<<1)
+#define BLUE_LED  (1<<2)
+#define SW1       (1<<4)
+#define SW2       (1<<0)
 
-#define enable 1
-#define clk_source (1<<2)
-#define INTEN (1<<1)
-#define red_led (1<<1)
-#define blue_led (1<<2)
-#define sw_1 (1<<4)
-
-
-void systick_handler(void){
-    GPIO_PORTF_DATA_R ^= red_led;
-
+void GPIOF_Handler(void){
+    int i;
+    GPIO_PORTF_ICR_R |= 1;
+    GPIO_PORTF_DATA_R |= BLUE_LED;
+    for (i = 0; i < 640000; i++) {
+        ;
+    }
+    GPIO_PORTF_DATA_R &= ~BLUE_LED;
 }
 
-void main(void)
-{
-    SYSCTL_RCGC2_R |=   0x00000020;
-   GPIO_PORTF_LOCK_R = 0x4C4F434B;
-   GPIO_PORTF_CR_R = 0x1F;
-   GPIO_PORTF_DEN_R = 0x1F;
-   GPIO_PORTF_DIR_R = 0X0E;
-   GPIO_PORTF_PUR_R = 0X11;
-   GPIO_PORTF_DATA_R = 0x0;
-   stcurrent = 0x0;
-   stctrl =0x0;
-   streload =0xFFFFFF;
-   stctrl |=(enable | clk_source | INTEN);
 
+int main(void){
+    SYSCTL_RCGC2_R |= 0x20;          // enable clock for Port F
+    GPIO_PORTF_LOCK_R = 0x4C4F434B;  // unlock PF0
+    GPIO_PORTF_CR_R   = 0x1F;        // allow changes on PF0–PF4
+    GPIO_PORTF_DEN_R  = 0x1F;        // enable digital for PF0–PF4
+    GPIO_PORTF_DIR_R  = (RED_LED | BLUE_LED); // LEDs output, switches input
+    GPIO_PORTF_PUR_R  = (SW1 | SW2); // pull-up on switches
+    GPIO_PORTF_DATA_R = 0x00;        // all off initially
 
-      while (1);
+      GPIO_PORTF_IM_R = 0;
+      GPIO_PORTF_IS_R = 0;
+      GPIO_PORTF_IEV_R =1;
+      GPIO_PORTF_ICR_R=1;
+      GPIO_PORTF_IM_R = 1;
 
+    NVIC_EN0_R |= (1<<30);
+
+    while(1){
+
+    }
 }
--- tm4c123gh6pm_startup_ccs.c
+++ tm4c123gh6pm_startup_ccs.c
@@ -33,7 +33,8 @@ void ResetISR(void);
 static void NmiSR(void);
 static void FaultISR(void);
 static void IntDefaultHandler(void);
-static void systick_handler(void);
+static void GPIOF_Handler(void);
+
 //*****************************************************************************
 //
 // External declaration for the reset handler that is to be called when the
@@ -82,7 +83,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // Debug monitor handler
     0,                                      // Reserved
     IntDefaultHandler,                      // The PendSV handler
-    systick_handler,                      // The SysTick handler
+    IntDefaultHandler,                      // The SysTick handler
     IntDefaultHandler,                      // GPIO Port A
     IntDefaultHandler,                      // GPIO Port B
     IntDefaultHandler,                      // GPIO Port C
@@ -113,7 +114,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // Analog Comparator 2
     IntDefaultHandler,                      // System Control (PLL, OSC, BO)
     IntDefaultHandler,                      // FLASH Control
-    IntDefaultHandler,                      // GPIO Port F
+    GPIOF_Handler,                      // GPIO Port F
     IntDefaultHandler,                      // GPIO Port G
     IntDefaultHandler,                      // GPIO Port H
     IntDefaultHandler,                      // UART2 Rx and Tx
