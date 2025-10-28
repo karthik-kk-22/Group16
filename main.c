245cab6319397b1ae2623e336c43728202162c3d
karthik <220020026@iitdh.ac.in>
2025-10-28 18:44:58 +0530

    Lab 11

--- main.c
+++ main.c
@@ -1,28 +1,15 @@
+#include "uart_ee627.h"
 #include <stdint.h>
-#include <stdbool.h>
-#include "tm4c123gh6pm.h"
-
-/**
- * main.c
- */
-
-int series_sum(int k){
-    volatile int arr[10];
-    int i=0;
-    for (i=0;i<10;i++){
-        arr[i]=i;
-    }
-    if (k<=1){
-        return 1;
-    }
-    else{
-        return k+series_sum(k-1);
-    }
-}
 
 int main(void)
-{   int a = 6;
-    int sum = series_sum(a);
-    while(1);
-	return 0;
+{
+    UART_setup();  // Initialize UART4
+
+    while (1)
+    {   int i;
+        UART_send(0x55);
+        for (i = 0; i < 1000000; i++);
+        UART_send(0xAA);
+        for (i = 0; i < 1000000; i++);
+    }
 }
